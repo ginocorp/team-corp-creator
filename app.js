993724ed3ea102ff0cc09797617bbd944d9731c6
Gino Colman <gdcolman95@gmail.com>
2022-03-18 01:32:56 -0400

    developed html templates and installed dependencies

---
+++ app.js
@@ -0,0 +1,63 @@
+// Internal Modules
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const render = require("./lib/render");
+const Questions = require("./questions");
+
+// External Packages
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const { inherits } = require("util");
+const { start } = require("repl");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+var employees = [];
+const q = new Questions
+
+
+// start of application
+async function init() {
+    try {
+        const startApp = await q.startQuestion();
+        if(startApp.startGame)
+            var keepRunning = true
+            while (keepRunning) {
+                var empRole = await q.employeeRole();
+                const empData = await q.employeeQuestions();
+                switch(empRole.listEmp) {
+                    case 'Manager':
+                        var roleData = await q.managerQuestions();
+                        newEmployee = new Manager(empData.empName, empData.empId, empData.empEmail, roleData.mgrOffice);
+                    break;
+                    case 'Engineer':
+                        var roleData = await q.engineerQuestions();
+                        newEmployee = new Engineer(empData.empName, empData.empId, empData.empEmail, roleData.github);
+                    break;
+                    case 'Intern':
+                        var roleData = await q.internQuestions();
+                        newEmployee = new Intern(empData.empName, empData.empId, empData.empEmail, roleData.schoolName);
+                    break;
+                }
+                employees.push(newEmployee)
+                console.log(employees);
+                var status = await q.confirmEmployee();
+                keepRunning = status.confirmEmp
+            }
+        } catch (error) {
+    console.log(error);
+    };
+// gets all inputs and renders the HTML
+    try {
+        let renderedHTML = render(employees);
+        fs.writeFileSync(outputPath, renderedHTML);
+        console.log('Finished! Check out your HTML page in the output folder.')
+    } catch (error) {
+        console.log(error);
+    }
+
+};
+init();
